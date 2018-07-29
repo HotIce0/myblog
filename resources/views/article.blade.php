f89e5cac34a1b75e20f83c634e884a9254ad4b2c
saoguang <saoguang@vip.qq.com>
2018-07-29 17:35:15 +0800

    新增，分享按钮

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -48,9 +48,8 @@
                         <a href="#">{{$data['label'][$i]}}</a>{{$i!=(count($data['label'])-1)?',':''}}
                     @endfor
                     <hr>
-                    <a href=""><span class="am-icon-qq am-icon-fw am-primary blog-icon"></span></a>
-                    <a href=""><span class="am-icon-wechat am-icon-fw blog-icon"></span></a>
-                    <a href=""><span class="am-icon-weibo am-icon-fw blog-icon"></span></a>
+                    <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a></div>
+                    <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
                 </div>
             </div>
             <hr>
